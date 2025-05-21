bb88d5d594e5a62085a84278af8598645bea5176
XDRayLl <rayli0413@gmail.com>
2025-05-22 00:03:22 +0800

    code

---
+++ Assets/WaveSpeed_Arduino.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.Android;
+using System.IO.Ports;
+using System;
+
+public class WaveSpeed_Arduino : MonoBehaviour
+{
+    public SerialPort sp = new SerialPort("com5", 38400);//Mega com5
+    //public SerialPort sp4 = new SerialPort("com4", 38400);
+    private Thread serialThread;
+    public float WaveVector;
+    public string wavespeed;
+
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        try
+        {
+            sp.Open();
+            //sp4.Open();
+            serialThread = new Thread(ReadSerialData);
+            serialThread.Start();
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError("Failed to open Serial Port: " + e.Message);
+        }
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void ReadSerialData()
+    {
+        while (true)
+        {
+            if (sp.IsOpen)
+            {
+                wavespeed = sp.ReadLine();
+                WaveVector = float.Parse(wavespeed);
+                Debug.Log("wavespeed:" + WaveVector);
+            }
+            Thread.Sleep(10); // 控制讀取頻率，避免過度占用CPU
+        }
+    }
+}
