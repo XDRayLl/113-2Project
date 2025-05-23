4dcf314378726e6d71c1fa825d458eecbaf341d1
XDRayLl <rayli0413@gmail.com>
2025-05-23 17:11:06 +0800

    Countgap

--- Assets/WaveSpeed_Arduino.cs
+++ Assets/WaveSpeed_Arduino.cs
@@ -13,6 +13,9 @@ public class WaveSpeed_Arduino : MonoBehaviour
     private Thread serialThread;
     public float WaveVector;
     public string wavespeed;
+    public float basic =0;
+    public float gap=0;
+
 
     // Start is called before the first frame update
 
@@ -52,6 +55,9 @@ public class WaveSpeed_Arduino : MonoBehaviour
                 wavespeed = sp.ReadLine();
                 WaveVector = float.Parse(wavespeed);
                 Debug.Log("wavespeed:" + WaveVector);
+                gap = WaveVector - basic;
+                Debug.Log("Gap:" + gap);
+                basic = WaveVector;
             }
             Thread.Sleep(10); // 控制讀取頻率，避免過度占用CPU
         }
